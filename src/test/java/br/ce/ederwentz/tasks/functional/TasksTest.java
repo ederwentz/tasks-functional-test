2023f84c6077d582f4ccef008876daea5baea7b1
Eder Wentz <77546367+ederwentz@users.noreply.github.com>
2022-10-27 14:47:08 -0300

    update TasksTest.java
    update TasksTest.java

--- src/test/java/br/ce/ederwentz/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/ederwentz/tasks/functional/TasksTest.java
@@ -20,11 +20,11 @@ public class TasksTest {
 //		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.3:4444/wd/hub"), cap);
 //		driver.navigate().to("http://192.168.1.3:8001/tasks");
 		// server Windows
-		//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
-		//driver.navigate().to("http://localhost:8001/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+		driver.navigate().to("http://localhost:8001/tasks");
 		// Server Linux
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.113:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.113:8001/tasks");
+		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.113:4444/wd/hub"), cap);
+		//driver.navigate().to("http://192.168.1.113:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
