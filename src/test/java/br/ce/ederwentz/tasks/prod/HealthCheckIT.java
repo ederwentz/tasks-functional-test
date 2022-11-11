8040069643861ad52c45e57600e6c7796f645bca
Eder Wentz <77546367+ederwentz@users.noreply.github.com>
2022-11-11 19:04:45 -0300

    update HealthCheckIT.java
    update HealthCheckIT.java

--- src/test/java/br/ce/ederwentz/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/ederwentz/tasks/prod/HealthCheckIT.java
@@ -18,7 +18,10 @@ public class HealthCheckIT {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
 		try {
-		driver.navigate().to("http://localhost:9999/tasks");
+		// local
+		//driver.navigate().to("http://localhost:9999/tasks");
+		// servidor
+		driver.navigate().to("http://192.168.1.112:8004/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String version = driver.findElement(By.id("version")).getText();
 		//System.out.println(version);
